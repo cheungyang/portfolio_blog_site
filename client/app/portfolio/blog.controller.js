cf9d24b34fa30c2a50d65249547c4d8fed18953c
Alva Cheung <cheungyang@gmail.com>
2014-08-26 22:54:46 -0700

    animation of article done

--- client/app/portfolio/blog.controller.js
+++ client/app/portfolio/blog.controller.js
@@ -14,8 +14,36 @@ angular.module('portfolioBlogSiteApp')
 
     $scope.myData = {};
     $scope.myData.doClick = function() {
-      $('.blog-content').html($scope.article.content);
-      $('.blog-expandable').removeClass('expandable-collapse').addClass('expandable-expand')
-        .find('.glyphicon').toggle('display-none');
+      var $expandable = $('.blog-expandable');
+      var $texts = $('.blog-content, .blog-summary');
+      var $main = $('.blog-main');
+      var h;
+
+      $expandable.toggleClass('display-none')
+        .toggleClass('expandable-collapse')
+        .toggleClass('expandable-expand')
+        .find('.glyphicon').toggleClass('display-none');
+
+      $main.css('height', $main.height());
+      $texts.each(function($text){
+        debugger;
+        var $that = $(this);
+        if ($that.hasClass('display-none')) {
+          $that.addClass('height-calc').removeClass('display-none');
+          h = $that.height();
+          setTimeout(function(){
+            $that.removeClass('height-calc')
+          }, 0);
+        } else {
+          $that.addClass('display-none');
+        }
+      });
+
+      // Re-show expandable when animation done
+      setTimeout(function(){
+        $main.css('height', h+'px');
+        $expandable.toggleClass('display-none');
+      }, 10);
+
     }
   });
